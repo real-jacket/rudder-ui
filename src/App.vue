39c5852beb446b653ccac9141b788e4cc5e079f6
jacket <1762982273@qq.com>
2020-09-30 16:34:29 +0800

    init project

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>hi</div>
+  <ke></ke>
+</template>
+
+<script>
+import Ke from './components/Ke.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Ke
+  }
+}
+</script>
---
+++ src/components/Ke.vue
@@ -0,0 +1,33 @@
+<template>
+    <input v-model="value"/>
+    <button @click="add">添加</button>
+    <ul>
+        <li v-for="(item,index) in list" :key='index + item'>
+            <span>{{item}}</span>
+            <span @click="remove(index)"> X </span>
+        </li>
+    </ul>
+</template>
+<script>
+export default {
+    data(){
+        return {
+            value:'',
+            list:[]
+        }
+    },
+    methods:{
+        add(){
+            if (!this.value) {
+                return
+            }
+            this.list.push(this.value)
+            this.value = ''
+        },
+        remove(index){
+            this.list.splice(index,1)
+            
+        }
+    }
+}
+</script>
