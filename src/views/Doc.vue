f921448c53188841115d127cc973701a5f3e4a29
jacket <1762982273@qq.com>
2020-09-30 21:02:10 +0800

    aside css

--- src/views/Doc.vue
+++ src/views/Doc.vue
@@ -1,7 +1,23 @@
 <template>
 <TopNav />
 <div class="content">
-    <aside class="aside">侧边栏</aside>
+    <aside class="aside">
+        <h2>组件列表</h2>
+        <ol>
+            <li>
+                <router-link to="/doc/switch">Switch 组件</router-link>
+            </li>
+            <li>
+                <router-link to="/doc/switch">Button 组件</router-link>
+            </li>
+            <li>
+                <router-link to="/doc/switch">Dialog 组件</router-link>
+            </li>
+            <li>
+                <router-link to="/doc/switch">Tabs 组件</router-link>
+            </li>
+        </ol>
+    </aside>
     <main class="main">主内容</main>
 </div>
 </template>
@@ -16,6 +32,31 @@ export default {
 }
 </script>
 
-<style scoped>
+<style lang="scss" scoped>
+.content {
+    display: flex;
 
+    aside {
+        background: lightblue;
+        min-width: 150px;
+        padding: 16px;
+        position: fixed;
+        top: 0;
+        left: 0;
+
+        >h2 {
+            margin-bottom: 4px;
+        }
+
+        >ol {
+            >li {
+                padding: 4px 0;
+            }
+        }
+    }
+
+    .main {
+        flex-flow: 1;
+    }
+}
 </style>
