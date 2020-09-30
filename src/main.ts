4261f2a44db76c809059a27da630b9a22b4a9586
jacket <1762982273@qq.com>
2020-09-30 20:39:38 +0800

    rm normalize.css and add home style

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,6 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import './index.scss'
-import 'normalize.css'
 import { createWebHashHistory, createRouter } from 'vue-router'
 import Home from './views/Home.vue'
 import Doc from './views/Doc.vue'
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,10 +1,10 @@
 <template>
 <TopNav />
 <div class="banner">
-    <h1>rudder ui</h1>
+    <h1>Rudder UI</h1>
     <h2>一个简单的轮子</h2>
     <p class="actions">
-        <a>Github</a>
+        <a href="https://github.com/real-jacket/rudder-ui" target="_blank">Github</a>
         <router-link to="/doc">起步</router-link>
     </p>
 </div>
@@ -20,6 +20,34 @@ export default {
 }
 </script>
 
-<style scoped> 
+<style lang="scss" scoped>
+.banner {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    height: 100%;
+    width: 100%;
+    background: #3eaf7c;
+    padding: 100px 0;
 
+    h1,
+    h2 {
+        margin: 10px 0;
+        letter-spacing: 5px;
+    }
+
+    .actions {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+
+        * {
+            background: #dddddd;
+            border-radius: 8px;
+            padding: 5px 15px;
+            margin: 10px;
+        }
+    }
+}
 </style>
