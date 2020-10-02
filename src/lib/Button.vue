520364ac3c7c47728c03f7ece95fba27b4154178
jacket <1762982273@qq.com>
2020-10-02 11:34:43 +0800

    button add level

--- src/lib/Button.vue
+++ src/lib/Button.vue
@@ -15,16 +15,22 @@ export default {
         size: {
             type: String,
             default: 'normal'
+        },
+        level: {
+            type: String,
+            default: 'normal'
         }
     },
     setup(props, context) {
         const {
             theme,
-            size
+            size,
+            level
         } = props
         const classes = {
             [`rudder-theme-${theme}`]: theme,
-            [`rudder-size-${size}`]: size
+            [`rudder-size-${size}`]: size,
+            [`rudder-level-${level}`]: level
         }
 
         return {
@@ -40,6 +46,7 @@ $border-color: #d9d9d9;
 $color: #333;
 $blue: #40a9ff;
 $radius: 4px;
+$red:red;
 
 .rudder-button {
     height: $h;
@@ -54,6 +61,7 @@ $radius: 4px;
     border: 1px solid $border-color;
     box-shadow: 0 1px 0 fade-out(black, 0.95);
     cursor: pointer;
+    transition: background 250ms;
 
     &+& {
         margin-left: 8px;
@@ -105,6 +113,67 @@ $radius: 4px;
             font-size: 12px;
             height: 20px;
             padding: 0 4px;
+        }
+    }
+
+    &.rudder-theme-button {
+        &.rudder-level-main {
+            background: $blue;
+            color: white;
+            border-color: $blue;
+
+            &:hover,
+            &:focus {
+                background: darken($blue, 10%);
+                border-color: darken($blue, 10%);
+            }
+        }
+
+        &.rudder-level-danger {
+            background: $red;
+            color: white;
+            border-color: $red;
+
+            &:hover,
+            &:focus {
+                background: darken($red, 10%);
+                border-color: darken($red, 10%);
+            }
+        }
+    }
+
+    &.rudder-theme-link {
+        &.rudder-level-main {
+            color: $blue;
+        }
+
+        &.rudder-level-danger {
+            color: $red;
+
+            &:hover,
+            &:focus {
+                color: darken($red, 10%);
+            }
+        }
+    }
+
+    &.rudder-theme-text {
+        &.rudder-level-main {
+            color: $blue;
+
+            &:hover,
+            &:focus {
+                color: darken($blue, 10%);
+            }
+        }
+
+        &.rudder-level-danger {
+            color: $red;
+
+            &:hover,
+            &:focus {
+                color: darken($red, 10%);
+            }
         }
     }
 }
--- src/views/ButtonDemo.vue
+++ src/views/ButtonDemo.vue
@@ -12,6 +12,24 @@
     <Button size="big">你好</Button>
     <Button size="small">你好</Button>
 </div>
+<h1>示例3</h1>
+<div>
+    <dir>
+        <Button level="main">主要按钮</Button>
+        <Button>一般按钮</Button>
+        <Button level="danger">危险按钮</Button>
+    </dir>
+    <dir>
+        <Button theme='link' level="main">主要链接按钮</Button>
+        <Button theme='link'>一般链接按钮</Button>
+        <Button theme='link' level="danger">危险链接按钮</Button>
+    </dir>
+    <dir>
+        <Button theme='text' level="main">主要文字按钮</Button>
+        <Button theme='text'>一般文字按钮</Button>
+        <Button theme='text' level="danger">危险文字按钮</Button>
+    </dir>
+</div>
 </template>
 
 <script lang="ts">
