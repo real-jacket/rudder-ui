7f036009d1eabfebeed1f31d90b556a112b869f7
jacket <1762982273@qq.com>
2020-10-03 16:00:07 +0800

    tabs tab css

--- src/lib/Tabs.vue
+++ src/lib/Tabs.vue
@@ -1,9 +1,12 @@
 <template>
-<div class="rudder-tabs-wrapper">
-    这是 tabs 组件
-    <div class="rudder-tab" v-for="(t,index) in titles" :key="index">{{t}}</div>
+<div class="rudder-tabs">
+    <div class="rudder-tabs-nav">
+        <div class="rudder-tabs-nav-item" v-for="(t,index) in titles" :key="index">{{t}}</div>
+    </div>
+    <div class="rudder-tabs-content">
+        <component v-for="(c,index) in children" :key="index" :is="c" />
+    </div>
 </div>
-<component v-for="(c,index) in children" :key="index" :is="c" />
 </template>
 
 <script lang="ts">
@@ -29,5 +32,33 @@ export default {
 </script>
 
 <style lang="scss">
+$blue: #40a9ff;
+$color: #333;
+$border-color: #d9d9d9;
 
+.rudder-tabs {
+    &-nav {
+        display: flex;
+        color: $color;
+        border-bottom: 1px solid $border-color;
+
+        &-item {
+            padding: 8px 0;
+            margin: 0 16px;
+            cursor: pointer;
+
+            &:first-child {
+                margin-left: 0;
+            }
+        }
+
+        &.selected {
+            color: $blue;
+        }
+    }
+
+    &-content {
+        padding: 8px 0;
+    }
+}
 </style>
