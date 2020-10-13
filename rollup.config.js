cb652588c6edd02ffad3d577e7b623206eb4aee4
jacket <1762982273@qq.com>
2020-10-13 22:07:05 +0800

    componet surpport esm and modify package.json

--- rollup.config.js
+++ rollup.config.js
@@ -6,7 +6,7 @@ import { terser } from "rollup-plugin-terser"
 
 export default {
   input: 'src/lib/index.ts',
-  output: {
+  output: [{
     globals: {
       vue: 'Vue'
     },
@@ -14,7 +14,12 @@ export default {
     file: 'dist/lib/rudder.js',
     format: 'umd',
     plugins: [terser()]
-  },
+  },{
+    name: 'Rudder',
+    file: 'dist/lib/rudder.esm.js',
+    format: 'es',
+    plugins: [terser()]
+  }],
   plugins: [
     scss({ include: /\.scss$/, sass: dartSass }),
     esbuild({
