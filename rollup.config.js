56e8ffa111b3e6b37da325dfb1e3ecc4a7d03e38
jacket <1762982273@qq.com>
2020-10-13 20:55:59 +0800

    componets add  rollup config modify packgae.json

---
+++ rollup.config.js
@@ -0,0 +1,29 @@
+import esbuild from 'rollup-plugin-esbuild'
+import vue from 'rollup-plugin-vue'
+import scss from 'rollup-plugin-scss'
+import dartSass from 'sass';
+import { terser } from "rollup-plugin-terser"
+
+export default {
+  input: 'src/lib/index.ts',
+  output: {
+    globals: {
+      vue: 'Vue'
+    },
+    name: 'Rudder',
+    file: 'dist/lib/rudder.js',
+    format: 'umd',
+    plugins: [terser()]
+  },
+  plugins: [
+    scss({ include: /\.scss$/, sass: dartSass }),
+    esbuild({
+      include: /\.[jt]s$/,
+      minify: process.env.NODE_ENV === 'production',
+      target: 'es2015' 
+    }),
+    vue({
+      include: /\.vue$/,
+    })
+  ],
+} 
